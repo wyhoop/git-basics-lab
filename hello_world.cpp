2ead572bec6aaff369eac5c8ac28f12a63701e16
Wyatt Hooper <wyhoop@cs.csis.work>
2023-09-05 11:14:41 -0500

    Added lines of code that performed basic arithmetic and output the result

--- hello_world.cpp
+++ hello_world.cpp
@@ -7,5 +7,10 @@ int main()
     cout << "Hello world." << endl;
     cout << "Making Changes." << endl;
 
+    int a;
+
+    a = 2 + 4;
+    cout << "Value of A is: " << a << endl;
+
     return 0;
 }
